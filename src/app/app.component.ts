e65fbfa65b4408ac99c6ce791d1a478ee3e44c75
Osnayder Severiche <osna.sev@gmail.com>
2021-09-27 07:54:39 -0500

    primer commit del curso de fundamentos de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({//Este decorador le dice a angular como debe comportarse esta clase, su template y los estilos
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-storagte';
+  name = 'Osnayder';
+  age = 32;
+  img = 'https://picsum.photos/200/300';
+  btnDisabled = true;
+  person = {
+    name:'Osnayder',
+    age:18,
+    avatar:'https://picsum.photos/200/300'
+  };
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.person.age++;
+  }
 }
---
+++ src/app/recap.ts
@@ -0,0 +1,13 @@
+// Declaración e inicialización de una variable
+const userName: string = 'nicobytes'
+
+const sum = (a:number, b:number) => {
+  return a +b;
+}
+sum (1,2)
+
+class Person {
+  constructor(public age:number, public lastName: string) {}
+}
+
+const os = new Person(32, 'Molina')
