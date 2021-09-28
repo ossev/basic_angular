ad3e6852e3813183c1ad39e3a5d7aed21ffc7db8
Osnayder Severiche <osna.sev@gmail.com>
2021-09-27 23:03:48 -0500

    Enviando el primer commit desde windows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './produc.model';
 
 @Component({//Este decorador le dice a angular como debe comportarse esta clase, su template y los estilos
   selector: 'app-root',
@@ -16,11 +17,72 @@ export class AppComponent {
     avatar:'https://picsum.photos/200/300'
   };
 
+  names: string[] = [
+    'Nombre1',
+    'Nombre2',
+    'Nombre3',
+    'Nombre4',
+  ]
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: 'https://picsum.photos/200/300',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'https://picsum.photos/200/300'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: 'https://picsum.photos/200/300'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: 'https://picsum.photos/200/300'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: 'https://picsum.photos/200/300'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: 'https://picsum.photos/200/300'
+    }
+  ]
+
+  newName = '';
   toggleButton(){
     this.btnDisabled = !this.btnDisabled;
   }
 
   increaseAge(){
     this.person.age++;
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+
+  addName(){
+    this.names.push(this.newName)
+    this.newName = ''
+  }
+
+  deleteName(index: number){
+    this.names.splice(index,1)
   }
 }
